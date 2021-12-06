7320956f7b6146194999a1012c848ae51b90e921
JRejowski <jan.rejowski@student.pk.edu.pl>
2021-12-06 10:26:18 +0100

    Lab3 exercises - wskaźniki/referencje

--- Lab3/src/func3.cpp
+++ Lab3/src/func3.cpp
@@ -4,10 +4,10 @@
 #include "func3.h"
 #include <stdlib.h>
 
+
 int getrandomint(int a, int b) {
     return a+rand()%(b-a+1);
 }
-
 int highestValueInArray(int *tab, int size) {
     int high=INT_MIN;
     for(int i=0;i<size;i++)
@@ -16,14 +16,10 @@ int highestValueInArray(int *tab, int size) {
         {
             high = tab[i];
         }
-
     }
-
     return high;
 }
-
 void changeOrder(int *tab, int size) {
-
     int *buffer = new int[size];
     for(int i=0; i<size; i++)
     {
@@ -36,4 +32,42 @@ void changeOrder(int *tab, int size) {
 
 }
 
+double calculateAverage(double *a, double *b) {
+    return  (*a + *b)/2;
+}
 
+void printPositive(int *tab, int size) {
+    for(int i=0;i<size;i++)
+    {
+        if(tab[i]>0)
+        {
+            cout<<tab[i]<<" ";
+        }
+    }
+    cout<<endl;
+
+}
+
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+
+void sort(int *tab, int size)
+{
+    int i, j;
+    for (i = 0; i < size-1; i++)
+
+        // Last i elements are already in place
+        for (j = 0; j < size-i-1; j++)
+            if (tab[j] > tab[j+1])
+                swap(&tab[j], &tab[j+1]);
+}
+
+void insertValueToVariable(int x, int *w) {
+
+    *w = x;
+}
--- Lab3/src/main.cpp
+++ Lab3/src/main.cpp
@@ -12,27 +12,45 @@ using namespace std;
 
 int main()
 {
+    double a=2;
+    double b=4;
+
+    int w;
+
     srand(time(NULL));
     int *tab = new int[100];
     for(int i=0;i<100;i++)
     {
         tab[i]= getrandomint(1,100);
+        tab[i]= getrandomint(-100,100);
     }
+    for(int i=0; i<100; i++)
+    {
+        cout<<tab[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"Highest number in array: "<<highestValueInArray(tab,100)<<endl;
+    changeOrder(tab,100);
     for(int i=0; i<100; i++)
     {
         cout<<tab[i]<<" ";
     }
 
     cout<<endl;
-    cout<<"Highest number in array: "<<highestValueInArray(tab,100)<<endl;
+    cout<<calculateAverage(&a,&b);
+    cout<<endl;
+    printPositive(tab,100);
 
-    changeOrder(tab,100);
+    sort(tab,100);
 
     for(int i=0; i<100; i++)
     {
         cout<<tab[i]<<" ";
     }
+    insertValueToVariable(10,&w);
 
+    cout<<endl;
+    cout<<w;
 
     return 0;
 }
